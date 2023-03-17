22f39432efdbc5c2879c16d5f03e5df797d282a8
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-03-17 16:04:53 +0000

    set up rounting user/admin

---
+++ app/(admin)/head.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function head() {
+  return (
+    <>
+      <title>Daily Blog</title>
+      <meta content="width=device-width, initla-scale=1" name="viewport" />
+      <link rel="icon" href="/fav.ico"></link>
+    </>
+  )
+}
+
+export default head
